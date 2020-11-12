0e6102ebfdd4c24211f5567e21339bcd666d81be
claireyywang <22240514+claireyywang@users.noreply.github.com>
2020-11-11 18:13:24 -0800

    add draft version CreateModelReview
    Signed-off-by: claireyywang <22240514+claireyywang@users.noreply.github.com>

--- bundles/models/model_review.go
+++ bundles/models/model_review.go
@@ -7,7 +7,25 @@ import (
 
 // contains information to create a model review
 type ModelReview struct {
+	// information in a reveiw
 	reviews.Review
-
+	// information in a model
 	Model
+}
+
+type CreateModelReview struct {
+	// relay all fields from CreateModel struct
+	CreateModel
+
+	// the branch where the model is on
+	Branch *string `json:"branch,omitempty"`
+
+	// reviewers requested for this pull request
+	Reviewers []*string `gorm:"-" json:"reviewers,omitempty"`
+
+	// owner organization for this model review
+	Owner *string `json:"branch,omitempty"`
+
+	// creator of this pull request
+	Creator *string `json:"branch,omitempty"`
 }
