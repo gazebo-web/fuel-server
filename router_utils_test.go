f11f0b35d539cd4bb0c0b4fba56b0df1c4e60b75
Ian Chen <ichen@osrfoundation.org>
2020-12-11 23:47:11 -0800

    remove debug msgs
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>

--- router_utils_test.go
+++ router_utils_test.go
@@ -64,18 +64,14 @@ type customInitializer func(ctx context.Context)
 
 // setup helper function
 func setupWithCustomInitalizer(customFn customInitializer) {
-  fmt.Print(" setup custom ini\n ")
 	logger := ign.NewLoggerNoRollbar("test", ign.VerbosityDebug)
 	logCtx := ign.NewContextWithLogger(context.Background(), logger)
 	// Make sure we don't have data from other tests.
 	// For this we drop db tables and recreate them.
 	// cleanDBTables(logCtx)
-  fmt.Print(" tear down\n ")
 	packageTearDown(logCtx)
-  fmt.Print(" add default data\n ")
 	DBAddDefaultData(logCtx, globals.Server.Db)
 
-  fmt.Print(" custom fun\n ")
 	if customFn != nil {
 		customFn(logCtx)
 	}
@@ -89,7 +85,6 @@ func setupWithCustomInitalizer(customFn customInitializer) {
 
 	// Create the router, and indicate that we are testing
 	igntest.SetupTest(globals.Server.Router)
-  fmt.Print("done setup test\n ")
 }
 
 //////////////////////////////
