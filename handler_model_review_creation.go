b35a0b2085f23bf1eaddba70eedac39df9b2fdb9
Alejo Carballude <alejocarballude@gmail.com>
2021-01-15 17:10:29 -0300

    Address model review create structure.

--- handler_model_review_creation.go
+++ handler_model_review_creation.go
@@ -13,8 +13,8 @@ import (
 )
 
 // extract actual model review process
-func modelReviewFn(input interface{}, tx *gorm.DB, jwtUser *users.User, w http.ResponseWriter, r *http.Request) (*reviews.ModelReview, *ign.ErrMsg) {
-	owner := input.Owner
+func modelReviewFn(cm models.CreateModel, tx *gorm.DB, jwtUser *users.User, w http.ResponseWriter, r *http.Request) (*reviews.ModelReview, *ign.ErrMsg) {
+	owner := cm.Owner
 	if owner != "" {
 		// Ensure the passed in name exists before moving forward
 		_, em := users.OwnerByName(tx, owner, true)
@@ -27,7 +27,7 @@ func modelReviewFn(input interface{}, tx *gorm.DB, jwtUser *users.User, w http.R
 
 	// Create the review via the Reviews Service
 	rs := &reviews.Service{}
-	review, em := rs.CreateReview(r.Context(), tx, input, uuidStr, jwtUser)
+	review, em := rs.CreateReview(r.Context(), tx, cm, uuidStr, jwtUser)
 	if em != nil {
 		os.Remove()
 		return nil, em
@@ -49,24 +49,31 @@ func ModelReviewCreate(tx *gorm.DB, w http.ResponseWriter, r *http.Request) (int
 		return nil, &errMsg
 	}
 
-    // The input data structure for this handler should contain all fields required by both the
-    // `ModelCreate` and `ModelReviewCreate` functions.
-	// create CreateModelReview input from request
-	var cmr := reviews.CreateModelReview
+	// Block 1: Create model
+	var cm models.CreateModel
 	if em := ParseStruct(&cm, r, true); em != nil {
 		return nil, em
 	}
+	cm.Metadata = parseMetadata(r)
 
-	// construct CreateModel input form from CreateModelReview
-	cm := models.CreateModel{
-		cmr.ModelID // how to access all model info through just an ID?
+	// Call the model create function
+	model, em := modelFn(cm, tx, jwtUser, w, r)
+	if em != nil {
+		return nil, em
 	}
+
+	// Block 2: Create model review with the newly created model
+	// The input data structure for this handler should contain all fields required by both the
+	// `ModelCreate` and `ModelReviewCreate` functions.
+	// create CreateModelReview input from request
+	var cmr reviews.CreateModelReview
+	if em := ParseStruct(&cm, r, true); em != nil {
+		return nil, em
+	}
+	cmr.ModelID = &model.ID
+
 	// create the model
 	model, em := modelFn(cm, tx, jwtUser, w, r)
-
-	// Get CreateReview input from CreateModelReview
-	rm := cmr.CreateReview
-	review, em := reviewFn(rm, tx, jwtUser, w, r)
 
 	return modelReview, nil
 }
