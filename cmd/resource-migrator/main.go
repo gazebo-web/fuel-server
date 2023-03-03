9f5d52910ac9d06abdbe86f212de65d0ad9f9875
Marcos Huck <marcoshuck@ekumenlabs.com>
2023-03-03 19:05:32 -0300

    Improve support for parallel downloads
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- cmd/resource-migrator/main.go
+++ cmd/resource-migrator/main.go
@@ -18,21 +18,81 @@ import (
 	"github.com/schollz/progressbar/v3"
 	"log"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 	"time"
 )
 
+const (
+	maxParallelUploads = 20
+)
+
 func main() {
-	// Initialize database
-	cfg, err := gz.NewDatabaseConfigFromEnvVars()
-	if err != nil {
-		log.Fatalln("Failed to get MySQL database config:", err)
-	}
-	db, err := gz.InitDbWithCfg(&cfg)
-	if err != nil {
-		log.Fatalln("Failed to connect to MySQL database:", err)
-	}
+	s, db := setup()
 	defer gz.Close(db)
+
+	run(s, db)
+}
+
+func run(s storage.Storage, db *gorm.DB) {
+	// Upload all models available in the current instance
+	log.Println("Processing Models")
+	started := time.Now()
+
+	// Get the list of resources to upload for: Models
+	ms, modelListSize, err := getResourceToUpload[*models.Model](db)
+	if err != nil {
+		log.Panicln("Failed to get models:", err)
+	}
+
+	// Get the list of resources to upload for: Worlds
+	ws, worldListSize, err := getResourceToUpload[*worlds.World](db)
+	if err != nil {
+		log.Panicln("Failed to get worlds:", err)
+	}
+
+	// Define progress bar
+	c := make(chan uploadRequest, (modelListSize+worldListSize)*10)
+
+	// Requesting all models
+	log.Println("Processing Models")
+	requestUpload[*models.Model](c, ms, "models")
+
+	// Requesting all worlds
+	log.Println("Processing Worlds")
+	requestUpload[*worlds.World](c, ws, "worlds")
+
+	// Listen for exit signal from when all resources have been uploaded
+	exit := make(chan struct{}, 1)
+
+	// Begin parallel uploads and keep track of them using the progress bar. It will send a single item to the exit
+	// channel once it finished
+	bar := newProgressBar(modelListSize+worldListSize, exit)
+	for i := 0; i < maxParallelUploads; i++ {
+		go upload(c, s, bar)
+	}
+
+	// Listen for Interrupt and Terminate signals
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		select {
+		case sig := <-sigs:
+			log.Panicln("Signal received:", sig.String())
+		case <-exit:
+			log.Println("Models and Worlds were successfully migrated. Took:", time.Since(started).Seconds(), "seconds")
+			return
+		}
+	}
+}
+
+func setup() (storage.Storage, *gorm.DB) {
+	db, err := setupDB()
+	if err != nil {
+		log.Fatalln("Failed to set up to MySQL database conn:", err)
+	}
 
 	globals.ResourceDir = os.Getenv("FUEL_RESOURCE_DIR")
 
@@ -44,60 +104,71 @@ func main() {
 	// Initialize S3 config
 	s3session := session.Must(session.NewSession())
 	s := storage.NewS3v1(s3.New(s3session), s3manager.NewUploader(s3session), os.Getenv("AWS_S3_BUCKET"))
-
-	// Upload all models available in the current instance
-	log.Println("Processing Models")
-	before := time.Now()
-	err = uploadModels(s, db)
-	if err != nil {
-		log.Panicln("Failed to migrate models:", err)
-	}
-	log.Println("Models were successfully migrated. Took:", time.Since(before).Seconds(), "seconds")
-
-	// Upload all worlds
-	log.Println("Processing Worlds")
-	err = uploadWorlds(s, db)
-	if err != nil {
-		log.Panicln("Failed to migrate worlds:", err)
-	}
-	log.Println("Worlds were successfully migrated. Took:", time.Since(before).Seconds(), "seconds")
+	return s, db
 }
 
-// uploadWorlds uploads all the worlds found in the database.
-func uploadWorlds(storage storage.Storage, db *gorm.DB) error {
-	var list []worlds.World
-	if err := db.Model(&worlds.World{}).Find(&list).Error; err != nil {
-		return err
-	}
-	log.Printf("Uploading a total number of %d worlds\n", len(list))
-	bar := progressbar.Default(int64(len(list)), "Uploading worlds")
-	for _, world := range list {
-		w := world
-		_ = bar.Add(1)
-		err := uploadResources(context.Background(), storage, "worlds", &w)
-		if err != nil {
-			log.Println("Failed to upload resource")
-		}
-	}
-	return nil
+func newProgressBar(size int, exit chan struct{}) *progressbar.ProgressBar {
+	return progressbar.NewOptions(size,
+		progressbar.OptionSetPredictTime(false),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSetDescription("Uploading resources"),
+		progressbar.OptionSetWriter(os.Stderr),
+		progressbar.OptionSetWidth(10),
+		progressbar.OptionThrottle(65*time.Millisecond),
+		progressbar.OptionOnCompletion(func() {
+			exit <- struct{}{}
+		}),
+		progressbar.OptionSpinnerType(14),
+		progressbar.OptionFullWidth(),
+		progressbar.OptionSetRenderBlankState(true),
+	)
 }
 
-func uploadModels(storage storage.Storage, db *gorm.DB) error {
-	var list models.Models
-	if err := db.Model(&models.Model{}).Find(&list).Error; err != nil {
-		return err
+func setupDB() (*gorm.DB, error) {
+	// Initialize database
+	cfg, err := gz.NewDatabaseConfigFromEnvVars()
+	if err != nil {
+		return nil, err
 	}
-	log.Printf("Uploading a total number of %d models\n", len(list))
-	bar := progressbar.Default(int64(len(list)), "Uploading models")
-	for _, model := range list {
-		m := model
-		_ = bar.Add(1)
-		err := uploadResources(context.Background(), storage, "models", &m)
-		if err != nil {
-			log.Println("Failed to upload resource")
+	db, err := gz.InitDbWithCfg(&cfg)
+	if err != nil {
+		return nil, err
+	}
+	return db, err
+}
+
+type uploadRequest struct {
+	Kind     string
+	Resource res.Resource
+}
+
+func requestUpload[T res.Resource](c chan uploadRequest, items []T, kind string) {
+	for _, item := range items {
+		c <- uploadRequest{
+			Kind:     kind,
+			Resource: item,
 		}
 	}
-	return nil
+}
+
+func upload(c chan uploadRequest, storage storage.Storage, bar *progressbar.ProgressBar) {
+	for !bar.IsFinished() {
+		req := <-c
+		err := uploadResources(context.Background(), storage, req.Kind, req.Resource)
+		if err != nil {
+			continue
+		}
+		_ = bar.Add(1)
+	}
+}
+
+func getResourceToUpload[T res.Resource](db *gorm.DB) ([]T, int, error) {
+	var list []T
+	var model T
+	if err := db.Model(&model).Find(&list).Error; err != nil {
+		return nil, 0, err
+	}
+	return list, len(list), nil
 }
 
 func uploadResources(ctx context.Context, storage storage.Storage, kind string, r res.Resource) error {
@@ -105,12 +176,12 @@ func uploadResources(ctx context.Context, storage storage.Storage, kind string, 
 	if err != nil {
 		return err
 	}
-	// If the tip version is not the version 1, we should migrate all the older versions
+	// If the tip vecion is not the vecion 1, we should migrate all the older vecions
 	for v > 1 {
 		// Decrease by 1
 		v--
 
-		// Upload the resources for the current version
+		// Upload the resources for the current vecion
 		v, err = uploadResource(ctx, storage, kind, strconv.Itoa(v), r)
 		if err != nil {
 			return err
@@ -119,8 +190,8 @@ func uploadResources(ctx context.Context, storage storage.Storage, kind string, 
 	return nil
 }
 
-func uploadResource(ctx context.Context, storage storage.Storage, kind, version string, r res.Resource) (int, error) {
-	path, ver, em := res.GetZip(ctx, r, kind, version)
+func uploadResource(ctx context.Context, storage storage.Storage, kind, vecion string, r res.Resource) (int, error) {
+	path, ver, em := res.GetZip(ctx, r, kind, vecion)
 	if em != nil {
 		log.Printf("Failed to get zip file for %s: %s\n", kind, em.BaseError)
 
@@ -130,13 +201,13 @@ func uploadResource(ctx context.Context, storage storage.Storage, kind, version 
 	defer gz.Close(f)
 	if err != nil {
 		log.Printf("Failed to open zip file for %s: %s\n", kind, err)
-		log.Printf("Name: %s | Owner: %s | Version: %d | Path: %d\n", *r.GetName(), *r.GetOwner(), ver, path)
+		log.Printf("Name: %s | Owner: %s | Vecion: %d | Path: %d\n", *r.GetName(), *r.GetOwner(), ver, path)
 		return 0, err
 	}
 	err = storage.UploadZip(ctx, res.CastResourceToStorageResource(r, uint64(ver)), f)
 	if err != nil {
 		log.Printf("Failed to upload zip file for %s: %s\n", kind, err)
-		log.Printf("Name: %s | Owner: %s | Version: %d | Path: %d\n", *r.GetName(), *r.GetOwner(), ver, path)
+		log.Printf("Name: %s | Owner: %s | Vecion: %d | Path: %d\n", *r.GetName(), *r.GetOwner(), ver, path)
 		return 0, err
 	}
 	return ver, nil
